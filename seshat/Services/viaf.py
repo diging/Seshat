b276412822b33ca242f96b19d8d4aaa76ef1ecd2
Erick <erick.peirson@asu.edu>
2013-04-23 09:39:40 -0700

    Added viaf.py

---
+++ seshat/Services/viaf.py
@@ -0,0 +1,12 @@
+"""Interacts with Virtual International Authority File to retrieve concept URIs."""
+
+class concept_source:
+    """Interacts with the VIAF API."""
+
+
+def main():
+    print "Nothing to see here."
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
