2f1f6d72561f5ff998055393b371cca215520063
Vignesh <vkannan3@asu.edu>
2013-05-17 15:22:36 -0700

    Updating the conceptpower.py file
    Updated code for part of the issue #2

--- seshat/Services/conceptpower.py
+++ seshat/Services/conceptpower.py
@@ -1,12 +1,21 @@
 """Interacts with ConceptPower API to retrieve concept URIs, and (where necessary) add new concepts."""
 
-class concept_source:
-    """Interacts with the ConceptPower authority API."""
+import requests
+import urllib2
+import sys
 
-
+def searchWord(Word, Pos):
+    URL = "http://digitalhps-develop.asu.edu:8080/conceptpower/rest/ConceptLookup/"+Word+"/"+Pos+""
+    Proxy = "http://digitalhps-develop.asu.edu:8080" 
+    Data = urllib2.urlopen(URL).read()    
+    print Data
+    
 def main():
     print "Nothing to see here."
+    searchWord("Mike", "noun")
 
 if __name__ == '__main__':
     status = main()
     sys.exit(status)
+    
+    
