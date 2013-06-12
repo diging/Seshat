be3f96d0b62e4c66f526b066461a9762f7af76c5
Erick <erick.peirson@asu.edu>
2013-06-12 16:25:28 -0700

    Fixes for corpus export
    Now ignores files without PDFs, and ignores missing references text
    bitstream (not all texts have references).

--- seshat/Web/export.py
+++ seshat/Web/export.py
@@ -50,36 +50,40 @@ class ExportHandler(webapp2.RequestHandler):
             csv_file += headers + '\n'
             
             for paper in papers:
-                line = str(paper.id) + ".pdf|" + str(paper.id) + "-cocr.txt|" + str(paper.id) + "-references.txt"
-                line += ',"' + paper.title[0] + '"'
-                line += ',' + paper.date[0]
-                line += ',"' + paper.abstract[0].replace("\"", "") + '"'
-                line += ',"' + paper.citation[0]['journal'][0] + ' ' + paper.citation[0]['volume'][0] + ': ' + paper.citation[0]['pages'][0] + '"'
-                line += ',"' + paper.source[0]['source'][0] + '"'
-                line += ',' + str(paper.source[0]['uri'][0])
-                line += ',' + paper.date_digitized[0]
-                line += ',"' + paper.type[0] + '"'
-                line += ',' + paper.language[0]
-                line += ',http://hdl.handle.net/10776/3984'
-                line += ',"' + paper.rights[0]['rights'][0] + '"'
-                line += ',"' + paper.rights[0]['holder'][0] + '"'
-                
-                for id in paper.creators[0]:
-                    creator = objects.Creator(id)
-                    line += ',"' + creator.name + '"'
-                    line += ',' + str(creator.uri)
-                
-                csv_file +=  line + '\n'
+                if paper.pdf[0] != '':  # Fixes issue #31
+                    line = str(paper.id) + ".pdf|" + str(paper.id) + "-cocr.txt|" + str(paper.id) + "-references.txt"
+                    line += ',"' + paper.title[0] + '"'
+                    line += ',' + paper.date[0]
+                    line += ',"' + paper.abstract[0].replace("\"", "") + '"'
+                    line += ',"' + paper.citation[0]['journal'][0] + ' ' + paper.citation[0]['volume'][0] + ': ' + paper.citation[0]['pages'][0] + '"'
+                    line += ',"' + paper.source[0]['source'][0] + '"'
+                    line += ',' + str(paper.source[0]['uri'][0])
+                    line += ',' + paper.date_digitized[0]
+                    line += ',"' + paper.type[0] + '"'
+                    line += ',' + paper.language[0]
+                    line += ',http://hdl.handle.net/10776/3984'
+                    line += ',"' + paper.rights[0]['rights'][0] + '"'
+                    line += ',"' + paper.rights[0]['holder'][0] + '"'
+                    
+                    for id in paper.creators[0]:
+                        creator = objects.Creator(id)
+                        line += ',"' + creator.name + '"'
+                        line += ',' + str(creator.uri)
+                    
+                    csv_file +=  line + '\n'
             outzip.writestr(str(corpus_id) + ".csv", csv_file, compress_type=ZIP_STORED)
                 
             # Write files to zip, and send to blobstore.
 
             for paper in papers:
-                logging.error(paper.pdf[0])
-                
-                outzip.writestr(str(paper.id) + ".pdf", urlfetch.fetch(paper.pdf[0]).content,  compress_type=ZIP_STORED)
-                outzip.writestr(str(paper.id) + "-cocr.txt", urlfetch.fetch(paper.full_text[0]).content,  compress_type=ZIP_STORED)
-                outzip.writestr(str(paper.id) + "-references.txt", urlfetch.fetch(paper.references_text[0]).content,  compress_type=ZIP_STORED)
+                if paper.pdf[0] != '':  # Fixes issue #31
+                    logging.error(paper.pdf[0])
+                    
+                    outzip.writestr(str(paper.id) + ".pdf", urlfetch.fetch(paper.pdf[0]).content,  compress_type=ZIP_STORED)
+                    outzip.writestr(str(paper.id) + "-cocr.txt", urlfetch.fetch(paper.full_text[0]).content,  compress_type=ZIP_STORED)
+                    
+                    if paper.references_text[0] != '':  # Fixes issue #30
+                        outzip.writestr(str(paper.id) + "-references.txt", urlfetch.fetch(paper.references_text[0]).content,  compress_type=ZIP_STORED)
 
             outzip.close()
             outfile.seek(0)
